573b06c163797d6df9d1864a0983e3aa43c49cba
felpud0 <felipe.sanchezg@um.es>
2023-07-11 18:08:18 +0200

    db conn works!

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
+# DB config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db.init_app(app)
+#Create tables if they are not created yet
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
