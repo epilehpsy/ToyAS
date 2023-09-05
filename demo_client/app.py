9525f5500bf69630106f9fcebe74fb870d0581c9
felpud0 <felipe.sanchezg@um.es>
2023-09-05 19:18:06 +0200

    demo client created

--- demo_client/app.py
+++ demo_client/app.py
@@ -1,16 +1,15 @@
-# Serve html code that contains an iframe with an oauth request behind an image
-# with flask
-#
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 import requests
 import base64
 
 IP="localhost"
 PORT=7777
 
+users = 0
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 
 # Fill this vars with the data from the registered client in the AS
 client_id = "98cf34e2301a9d77c3a3e46e62cea8b153743cca9a5d23d5"
@@ -19,6 +18,14 @@ redirect_uri = "http://localhost:7777/callback"
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/connect')
+def connect():
+    pass
+
+@app.route('/clickjacking')
 def iframe():
     return '''
     <iframe style="position: absolute; top: 0; left: 0;  z-index: 1; opacity: 0.5;"
@@ -34,5 +41,4 @@ def callback():
     code = request.args.get('code')
     return "This is the authorization code: " + code
 
-if __name__ == '__main__':
-    app.run(host=IP, port=PORT, debug=True)
+
